8355e1834c329953af27b6adf6688cc716f03a2f
msky026 <pref.myu@gmail.com>
2017-12-30 08:34:32 +0900

    modify tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,11 +2,11 @@ class TweetsController < ApplicationController
   before_action :authenticate, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @tweets = current_user.tweets.all
+    @tweets = Tweet.all
   end
 
   def show
-    @tweet = current_user.tweets.find(params[:id])
+    @tweet = Tweet.find(params[:id])
     return redirect_to tweets_path if @tweet.pic.nil?
     pic = JSON.parse(@tweet.pic)
     @image_url = pic["url"] if pic
@@ -26,11 +26,11 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = current_user.tweets.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
-    @tweet = current_user.tweets.find(params[:id])
+    @tweet = Tweet.find(params[:id])
     if @tweet.update_with_image(current_user.id, tweet_params)
       redirect_to @tweet, notice: '更新しました'
     else
@@ -39,14 +39,14 @@ class TweetsController < ApplicationController
   end
 
   def destroy
-    @tweet = current_user.tweets.find(params[:id])
+    @tweet = Tweet.find(params[:id])
     @tweet.delete_with_image
     redirect_to root_path, notice: '削除しました'
   end
 
   def image
-    @user = current_user
-    @tweet = current_user.tweets.find(params[:id])
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
     pic = JSON.parse(@tweet.pic)
     @image_url = pic["url"] if pic
     render :image, layout: false
